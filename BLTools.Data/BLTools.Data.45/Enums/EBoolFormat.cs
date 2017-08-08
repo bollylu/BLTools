91c99c9b04d93e1f41846de3375c93597238cd68
bollylu <lbolly@sharenet.be>
2017-08-08 21:42:17 +0200

    Force rebuild of BLTools.Data.45

--- BLTools.Data/BLTools.Data.45/Enums/EBoolFormat.cs
+++ BLTools.Data/BLTools.Data.45/Enums/EBoolFormat.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BLTools.Data {
+  /// <summary>
+  /// Possible values for a string to be converted to bool
+  /// </summary>
   public enum EBoolFormat {
     YesOrNo,
     YOrN,
